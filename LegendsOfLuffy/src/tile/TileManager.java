7f36e5abfa852ff21ef6eaccb8dd26abe8be547a
schirica7 <stefanchirica2@gmail.com>
2023-01-15 15:03:42 -0500

    Stefan Last Commit For A While

--- LegendsOfLuffy/src/tile/TileManager.java
+++ LegendsOfLuffy/src/tile/TileManager.java
@@ -43,7 +43,7 @@ public class TileManager {
     public void getTileImage() {
         try {
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tileImages/ground.png"));
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tileImages/grass01.png"));
 
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tileImages/wall.png"));
